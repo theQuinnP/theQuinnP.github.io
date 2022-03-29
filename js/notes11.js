e4163590ef4979c780f36b0e20721365a248f160
theQuinnP <qupe8693@colorado.edu>
2022-03-29 14:35:48 -0600

    stuff

---
+++ js/notes11.js
@@ -0,0 +1,36 @@
+let myVariable;
+myVariable = a;
+
+let myVariable2 = 'b';
+
+myVariable2 = 'c';
+
+let myVariable = 'bob'; //strings
+
+let myNumber = 3; //numbers
+
+let myBoolean = true;
+let myBoolean = false;
+
+let myArray = [1, 'bob', true]; //arrays
+
+let myObject = decument.querySelector('p');
+
+
+// OPERATORS
+
+6 + 9; //addition
+'Hello' + 'World' //STRING ADDITION --> 'HelloWorld'
+
+// SUBTRACTION --> -
+// MULTIPLICATION --> *
+// DIVISION --> =
+
+// ASSIGNMENT --> =
+
+// EQUALITY -->
+myNumber === 4; // <-- this will result in false
+
+//NOT
+!(myNumber === 3); //!(true) = false
+myVariable !== 3; //false

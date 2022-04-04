b744572e0488fcb08ac58b33da64ef00d9f6dbf4
theQuinnP <qupe8693@colorado.edu>
2022-04-03 22:22:33 -0600

    finished

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,78 +1,19 @@
 
-const para = document.querySelector('p');
 
-para.addEventListener('mouseover', mOver1);
+  console.log('js is here');
 
-    function mOver1(e) {
-      e.target.src = "img/constellations/orion2.svg";
-    }
+  function button1Clicked(){
+    document.getElementById('button-1').classList.toggle('was-clicked1');
+  }
+  function windowLoaded(){
+    console.log('loaded');
+    document.getElementById('button-1').addEventListener('click', button1Clicked);
+    document.getElementById('button-2').addEventListener('mouseover', button2Hover);
+    document.getElementById('button-2').addEventListener('mouseout', button2Hover);
+  }
 
-    function mOut1(obj) {
-      obj.innerHTML = <img src = "img/constellations/orion1.svg" alt = "orion">
-    }
+  function button2Hover(){
+    document.getElementById('button-2').classList.toggle('was-clicked2');
+  }
 
-para.addEventListener('hover', mOver2);
-
-    function mOver2(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut2(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver3(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut3(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver4(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut4(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver5(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut5(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver6(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut6(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver7(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut7(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver8(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut8(obj) {
-      obj.innerHTML = "test 1"
-    }
-
-    function mOver9(obj) {
-      obj.innerHTML = "great"
-    }
-
-    function mOut9(obj) {
-      obj.innerHTML = "test 1"
-    }
+  window.onload = windowLoaded;
